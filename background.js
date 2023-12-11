22d5ccd70bc0976fad05353d30e51bba52352c5c
emmanuellion <emmanuel.lion30@gmail.com>
2023-12-11 23:13:23 +0100

    Support MacOS

--- background.js
+++ background.js
@@ -1,7 +1,54 @@
+function parseWindows(dataData){
+    let str = "";
+    const entries = dataData.data.threaded_conversation_with_injections_v2.instructions[0]?.entries;
+    let savedEntrys = null;
+    for (const entry of entries) {
+        let content = entry?.content?.itemContent?.tweet_results?.result?.legacy?.entities;
+        if(content === undefined)
+            content = entry?.content?.itemContent?.tweet_results?.result?.tweet.legacy.entities;
+        if (content?.media)
+            savedEntrys = content.media;
+    }
+    if (savedEntrys) {
+        for(let i = 0; i < savedEntrys.length; i++){
+            if(savedEntrys[i].type === "video"){
+                let video = "";
+                let biggest = 0;
+                const vars = savedEntrys[i].video_info.variants;
+                for(let j = 0; j < vars.length; j++) {
+                    if (vars[j].bitrate){
+                        if (vars[j].bitrate > biggest) {
+                            biggest = vars[j].bitrate;
+                            video = vars[j].url;
+                        }
+                    }
+                }
+                str += video;
+                if(i+1<savedEntrys.length)
+                    str += "<br/>"
+            }
+        }
+    }
+    return str;
+}
+
+function parseMac(dataData){
+    const medias = dataData.data.tweetResult.result.legacy.entities.media;
+    let str = "";
+    for(let media of medias){
+        str += media.media_url_https;
+        str += "<br/>";
+    }
+    console.log(dataData);
+    return str;
+}
+
+const urls = ["TweetDetail", "TweetResultByRestId"];
+
 let betterHeader = {}
 
 function getHeader(req){
-    if(req.url.includes('TweetDetail')){
+    if(urls.some(url => req.url.includes(url))){
         const headers = req.requestHeaders;
         for(let i = 0; i < headers.length; i++){
             if(!betterHeader[headers[i].name]) betterHeader[headers[i].name] = headers[i].value;
@@ -13,7 +60,7 @@ let dataData = null;
 let last = "";
 
 async function getData(req) {
-    if(req.url.includes('TweetDetail') && last !== req.url){
+    if(urls.some(url => req.url.includes(url)) && last !== req.url){
         last = req.url;
         const res = await fetch(req.url, {
             "method": "GET",
@@ -29,39 +76,14 @@ async function getData(req) {
         });
         dataData = await res.json();
         try {
-            const entries = dataData.data.threaded_conversation_with_injections_v2.instructions[0]?.entries;
-            let savedEntrys = null;
-            for (const entry of entries) {
-                let content = entry?.content?.itemContent?.tweet_results?.result?.legacy?.entities;
-                if(content === undefined)
-                    content = entry?.content?.itemContent?.tweet_results?.result?.tweet.legacy.entities;
-                if (content?.media)
-                    savedEntrys = content.media;
-            }
-            if (savedEntrys) {
-                let str = "";
-                for(let i = 0; i < savedEntrys.length; i++){
-                    if(savedEntrys[i].type === "video"){
-                        let video = "";
-                        let biggest = 0;
-                        const vars = savedEntrys[i].video_info.variants;
-                        for(let j = 0; j < vars.length; j++) {
-                            if (vars[j].bitrate){
-                                if (vars[j].bitrate > biggest) {
-                                    biggest = vars[j].bitrate;
-                                    video = vars[j].url;
-                                }
-                            }
-                        }
-                        str += video;
-                        if(i+1<savedEntrys.length)
-                            str += "<br/>"
-                    }
-                }
-                console.log(str);
-                if (str.trim().length > 1)
-                    chrome.storage.local.set({ 'twitter_url': str }, () => {});
-            }
+            let str = "";
+            if(req.url.includes('TweetDetail'))
+                str = parseWindows(dataData);
+            else
+                str = parseMac(dataData);
+            console.log(str);
+            if (str.trim().length > 1)
+                chrome.storage.local.set({ 'twitter_url': str }, () => {});
         } catch (e) {
             console.error(e);
         }
@@ -79,3 +101,4 @@ chrome.webRequest.onCompleted.addListener(
     { urls: ["<all_urls>"] },
     ["responseHeaders"]
 );
+
